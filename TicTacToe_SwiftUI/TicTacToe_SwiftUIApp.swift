70e14a6949b6d8aefb9db186fb2766304984388c
Niko Berdzenishvili <nikolozisia@gmail.com>
2023-02-16 19:15:47 +0400

    Initial Commit

---
+++ TicTacToe_SwiftUI/TicTacToe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_SwiftUIApp.swift
+//  TicTacToe_SwiftUI
+//
+//  Created by Niko on 16.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
